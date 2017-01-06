db11514d86c71afb7b593bd67e9fb607b1414ada
Maicon <maicon-gil@PRO-PAE-0483.PROCERGS.REDERS>
2017-01-06 09:56:59 -0200

    Add sinon lib

--- cow.js
+++ cow.js
@@ -12,6 +12,16 @@
       if (!target)
         throw new Error("missing target");
       return this.name + " greets " + target;
+    },
+
+    lateGreets: function(target, cb) {
+      setTimeout(function(self) {
+        try {
+          cb(null, self.greets(target));
+        } catch (err) {
+          cb(err);
+        }
+      }, 1000, this);
     }
   };
 })(this);
